f809655b7b38242bb0cd57e2c8778427620f1c6f
theo <theorestilo@gmail.com>
2022-08-17 22:16:21 -0300

    practica 1

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -4,10 +4,18 @@ function crearUsuario() {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
   // En el `contructor`, define el usuario, el nombre, el email y la contraseña
-  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 
+  //'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario (opciones){
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.contraseña = opciones.contraseña;
+  }
+  Usuario.prototype.saludar = function (){console.log('hola mi nombre es : ' + this.nombre)}
 }
 
 function agregarMetodoPrototype(Constructor) {
